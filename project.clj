ffcea767c53844fb207a21b2242dd1cc39e5909a
Dmytro Lyman <liman.dimitriy@gmail.com>
2016-10-03 23:07:57 +0300

    init project

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject pattern-detection "0.1.0-SNAPSHOT"
+  :description "tool for parsing source code and maybe detection design patterns"
+  :url "https://github.com/dimitriylol/pattern-detection"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [instaparse "1.4.1"]]
+  :main ^:skip-aot pattern-detection.core
+  :target-path "target/%s"
+  :resource-paths ["resources"]
+  :profiles {:uberjar {:aot :all}})
