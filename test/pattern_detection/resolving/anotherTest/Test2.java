e033b13a0834e427abb513f128f44e7d250c2a76
Podze Aleksandr <dasdeg@gmail.com>
2016-10-11 00:20:11 +0300

    dummy sources for testing id-resolver

---
+++ test/pattern_detection/resolving/anotherTest/Test2.java
@@ -0,0 +1,5 @@
+package anotherTest;
+
+interface ITest2 { }
+interface ITest extends test.ITest1 { } 
+public class Test2 extends test.anotherTest.Test2 { } 
---
+++ test/pattern_detection/resolving/test/anotherTest/Test2.java
@@ -0,0 +1,8 @@
+package test.anotherTest;
+
+interface ITest { }
+
+interface ITest1 { }
+class Test1 { }
+
+public class Test2 extends Test1 implements ITest, ITest1 { }
