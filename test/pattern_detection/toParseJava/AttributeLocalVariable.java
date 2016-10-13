c91b82da284172ee03525e3f1a0dd487e91a0e0e
Dmytro Lyman <liman.dimitriy@gmail.com>
2016-10-13 07:10:20 +0300

    [test] add test file for attribute/local variable

---
+++ test/pattern_detection/toParseJava/AttributeLocalVariable.java
@@ -0,0 +1,15 @@
+class AttributeLocalVariable {
+    long privateLong  ;
+    private long preciseLong;
+    public int publicInt , andOneMore
+        ;
+    static public int staticPublicInt = 0;
+
+    void localVariableMethod () {
+        float var;
+        boolean localVar, anotherLocalVar = true;
+        String localStr = "Life question";
+        char localChar = '?';
+        double initializedLocalVar = 42.0;
+    }
+}
