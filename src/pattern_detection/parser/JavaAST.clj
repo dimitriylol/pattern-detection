6f510f361c10608e43cb9bf93a300456e18087d0
Dmytro Lyman <liman.dimitriy@gmail.com>
2016-10-27 01:12:45 +0300

    move functions for java AST

---
+++ src/pattern_detection/parser/JavaAST.clj
@@ -0,0 +1,24 @@
+(ns pattern-detection.parser.JavaAST)
+
+(defn build-qid [package-id]
+  (fn [type-id] (str package-id "." type-id)))
+
+(defn get-id [id] (nth id 1))
+
+(defn get-type-id [type-decl]
+  (get-id (nth type-decl 2)))
+
+(defn get-class-id [class-decl] (get-type-id class-decl))
+
+(defn get-interface-id [interface-decl] (get-type-id interface-decl))
+
+(defn get-qid [qid-sort]
+  (nth qid-sort 1))
+
+(defn package-decl-p [grammar-sort]
+  (= :packageDecl (nth grammar-sort 0)))
+
+(defn get-package-id [comp-unit]
+  (if (package-decl-p (nth comp-unit 1))
+    (get-qid (nth (nth comp-unit 1) 1))
+    "$default"))
--- src/pattern_detection/uml_relationship/id_resolver.clj
+++ src/pattern_detection/uml_relationship/id_resolver.clj
@@ -1,45 +1,19 @@
 (ns pattern-detection.uml-relationship.id-resolver
   (:require [clojure.core.match :refer [match]]
-            [clojure.string :as cj-str]))
-
-(defn- build-qid [package-id]
-  (fn [type-id] (str package-id "." type-id)))
-
-(defn- get-id [id] (nth id 1))
-
-(defn- get-type-id [type-decl]
-  (get-id (nth type-decl 2)))
-
-(defn- get-class-id [class-decl] (get-type-id class-decl))
-
-(defn- get-interface-id [interface-decl] (get-type-id interface-decl))
-
-(defn- stop-elements [tree]
-  (or (keyword? tree) (string? tree)))
-
-(defn- stop-matcher [tree-traverser tree & traverser-args]
-  (if (or (empty? tree) (stop-elements (nth tree 0)))
-    '()
-    (mapcat #(apply tree-traverser % traverser-args) tree)))
-
-(defn- get-qid [qid-sort]
-  (nth qid-sort 1))
-
-(defn- package-decl-p [grammar-sort]
-  (= :packageDecl (nth grammar-sort 0)))
-
-(defn- get-package-id [comp-unit]
-  (if (package-decl-p (nth comp-unit 1))
-    (get-qid (nth (nth comp-unit 1) 1))
-    "$default"))
+            [clojure.string :as cj-str]
+            [pattern-detection.parser.JavaAST :refer :all]
+            [pattern-detection.uml-relationship.utils.traverser :refer [full-tree-matcher]]))
 
 (defn- gather-decls [tree builder-qid]
-  (match [tree]
-         [[:classDecl & children]]
-         (list {(builder-qid (get-class-id tree)) :class})
-         [[:interfaceDecl & children]]
-         (list {(builder-qid (get-interface-id tree)) :interface})
-         :else (stop-matcher gather-decls (rest tree) builder-qid)))
+  (full-tree-matcher
+   tree
+   (fn [local-tree]
+     (match [local-tree]
+            [[:classDecl & children]]
+            {(builder-qid (get-class-id local-tree)) :class}
+            [[:interfaceDecl & children]]
+            {(builder-qid (get-interface-id local-tree)) :interface}   
+            :else nil))))
 
 (defn build-resolver [forest]
   (apply merge (mapcat #(gather-decls (:tree %)
