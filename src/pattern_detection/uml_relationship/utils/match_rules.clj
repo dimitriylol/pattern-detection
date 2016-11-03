05e752a2c405af1670546f33d85c8526b0d2fd7b
Dmytro Lyman <liman.dimitriy@gmail.com>
2016-11-04 01:15:06 +0200

    [Java parser] generating matcher functions

--- src/pattern_detection/uml_relationship/utils/match_rules.clj
+++ src/pattern_detection/uml_relationship/utils/match_rules.clj
@@ -2,53 +2,34 @@
   (:require [clojure.core.match :refer [match]]
             [pattern-detection.uml-relationship.utils.traverser :refer [full-tree-matcher]]))
 
-(defn match-classes [tree]
-  (match [tree]
-         [[:classDecl & children]] true
-         :else nil))
+(defmacro matcher [grammar-sort]
+  `(fn [tree#]
+     (match [tree#]
+            [[~grammar-sort & children#]] tree#
+            :else nil)))
 
-(defn match-interfaces [tree]
-  (match [tree]
-         [[:interfaceDecl & children]] true
-         :else nil))
+(defmacro matcher-rule [rule]
+  `(fn [tree#]
+     (match [tree#]
+            [~rule] tree#
+            :else nil)))
 
-(defn match-methods [tree]
-  (match [tree]
-         [[:methodDecl & children]] true
-         :else nil))
+(defmacro match-decl-with-id [decl-sort id]
+  `(fn [tree#]
+     (match [tree#]
+            [[~decl-sort _# [:id ~id] & children#]] tree#
+            :else nil)))
 
-(defn match-methods-in-class [class-name]
-  (fn [tree]
-    (map
-     #(full-tree-matcher % match-methods)
-     (match [tree]
-            [[:classDecl _ [:id class-name] _ _ [:declBody & decls]]]
-            decls
-            :else nil))))
+(defn matcher-combination [matcher-applier tree & matcher-func]
+  (reduce (fn [res-forest matcher-fn]
+            (mapcat (fn [res-tree]
+                      (matcher-applier res-tree matcher-fn))
+                    res-forest))
+          (list tree)
+          matcher-func))
 
-(defn get-id [qid-id-sort]
-  (nth qid-id-sort 1))
-
-(defn match-class-extends [class-name]
-  (fn [tree]
-    (map get-id
-         (match [tree]
-                [[:classDecl _ [:id class-name] [:extends & qid] & children]]
-                qid
-                :else nil))))
-
-(defn match-class-implements [class-name]
-  (fn [tree]
-    (map get-id
-         (match [tree]
-                [[:classDecl _ [:id class-name] _ [:implements & qids] & children]]
-                qids
-                :else nil))))
-
-(defn match-interface-extends [interface-name]
-  (fn [tree]
-    (map get-id
-         (match [tree]
-                [[:interfaceDecl _ [:id interface-name] [:extends & qids] & children]]
-                qids
-                :else nil))))
+(defmacro matcher-decl-var [decl-var-id]
+  `(fn [tree#]
+     (match [tree#]
+            [[:declVar _# [:id ~decl-var-id] & children#]] tree#
+            :else nil)))
--- src/pattern_detection/uml_relationship/utils/traverser.clj
+++ src/pattern_detection/uml_relationship/utils/traverser.clj
@@ -1,10 +1,16 @@
 (ns pattern-detection.uml-relationship.utils.traverser)
 
-(defn full-tree-matcher [tree matcher-function]
+(defn- flatten-only-list [x]
+  (filter (complement seq?)
+          (rest (tree-seq seq? seq x))))
+
+(defn- full-tree-matcher-helper [tree matcher-function]
   (filter some?
           (mapcat (fn [local-tree]
                     (if (coll? local-tree)
                       (concat (list (matcher-function local-tree))
-                              (full-tree-matcher local-tree matcher-function))
-                      '()))
+                              (full-tree-matcher-helper local-tree matcher-function))))
                   tree)))
+
+(defn full-tree-matcher [tree matcher-function]
+  (flatten-only-list (full-tree-matcher-helper tree matcher-function)))
